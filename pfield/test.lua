d1b04f0f7b1a56ae5da599fb2c2a5286ac661f87
monsoon <97245589@qq.com>
2026-09-26 14:42:00 -0600

    del meta

--- pfield/test.lua
+++
@@ -1,9 +0,0 @@
-require "util"
-local parsemeta = require "parsemeta"
-local playermeta = require "playermeta"
-
-parsemeta.setmeta(playermeta)
-
-print(parsemeta.sproto("Player"))
-local obj = parsemeta.genobj("Player")
-print(dump(obj.Player))
